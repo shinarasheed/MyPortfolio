44b20e120c9045ad4d685c92a92b266b378d5eaf
shina <rasheedshinaopeyemi@gmail.com>
2021-05-24 09:02:39 +0100

    added more hero image info

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -27,9 +27,12 @@ const Hero = () => {
           <div>
             <div className="underline"></div>
             <h1>I'm Shina</h1>
-            <h4>Freelance Web and mobile UI/UX Designer</h4>
-            <Link to="/contact" className="btn">
-              contact me
+            <h4>I’m a full stack developer, specialized in the </h4>
+            <h4> development of web and mobile applications</h4>
+            <h4>using React.js, Next.js, Gatsby.js, Node.js,</h4>
+            <h4>React Native &amp; more</h4>
+            <Link to="/aboutme" className="btn">
+              Find Out More
             </Link>
             <SocialLinks />
           </div>
