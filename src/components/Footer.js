27465e075debda9eef905decf551989515bb1ac4
shina <rasheedshinaopeyemi@gmail.com>
2021-05-24 10:27:45 +0100

    added my social media addresses

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,25 @@
 import React from "react"
 import SocialLinks from "../constants/socialLinks"
 const Footer = () => {
+  // © 2021 John Smilga. All rights reserved. Built withGatsby
   return (
     <footer className="footer">
       <div>
         <SocialLinks styleClass="footer-links" />
         <h4>
-          copyright&copy; {new Date().getFullYear()}
-          <span>Webdev</span> all rights reserved
+          &copy; {new Date().getFullYear()} <span>shinarasheed</span>. All
+          rights reserved. Built with{" "}
+          <span style={{ color: "red", fontSize: "1.5rem" }}>&#9829; </span>{" "}
+          <span>
+            with{" "}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.gatsbyjs.com/"
+            >
+              gatsby
+            </a>
+          </span>
         </h4>
       </div>
     </footer>
