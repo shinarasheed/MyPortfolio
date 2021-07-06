09551ecf03e30f791c454098e233e5323747eea3
shina <rasheedshinaopeyemi@gmail.com>
2021-07-06 12:47:28 +0100

    adjusted the mobile screen font sizes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -27,7 +27,7 @@ const Hero = () => {
           <div>
             <div className="underline"></div>
             <h1>I'm Shina</h1>
-            <h4>I’m a Full Stack Software Engineer specialized </h4>
+            <h4>I’m a Full Stack Software Engineer skilled</h4>
             <h4>in the development of web and mobile</h4>
             <h4>applications using React.js, Next.js</h4>
             <h4>Node.js, GraphQL, React Native &amp; more</h4>
