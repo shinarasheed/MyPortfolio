732aa26f0d22fb2fab35ca2557654f9aa0759b95
shina <rasheedshinaopeyemi@gmail.com>
2021-07-06 16:18:31 +0100

    refactored

--- src/constants/services.js
+++ src/constants/services.js
@@ -5,7 +5,7 @@ const services = [
     id: 1,
     icon: <FaCode className="service-icon" />,
     title: "web development",
-    text: `I have more than 3 years of developing client facing web applications of various sizes and complexity using tools in the javaScript and PHP ecosystem`,
+    text: `I have more than 2 years of developing client facing web applications of various sizes and complexity using tools in the javaScript and PHP ecosystem`,
   },
   {
     id: 2,
